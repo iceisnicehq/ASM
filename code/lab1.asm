c057eb945ca8ebd5d513426a0ac0fc3ae5f1f88b
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-09-05 16:12:56 +0300

    Update code

---
+++ code/lab1.asm
@@ -0,0 +1,9 @@
+;pseudo code
+
+;defining a, b, c as ?
+; jumping to combinations if undefined 
+; else go to calculations
+; cycling through numbers
+; calculating 
+; jumping to writing routine if overflow
+; 
